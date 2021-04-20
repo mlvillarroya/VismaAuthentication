7d84f11ac04cc623ac8213bbd25515ded4182296
mlvillarroya <mlvillarroya@gmail.com>
2021-04-20 20:59:23 +0200

    Code cleaning

--- VismaAuthentication/VismaAuthentication/Models/GoogleResponseModel.cs
+++ VismaAuthentication/VismaAuthentication/Models/GoogleResponseModel.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace VismaAuthentication.Models
+﻿namespace VismaAuthentication.Models
 {
     public class GoogleResponseModel
     {
--- VismaAuthentication/VismaAuthentication/Views/AuthenticatorView.xaml.cs
+++ VismaAuthentication/VismaAuthentication/Views/AuthenticatorView.xaml.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using VismaAuthentication.ViewModels;
+﻿using VismaAuthentication.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using VismaAuthentication.ViewModels;
+
 
 namespace VismaAuthentication.Views
 {
