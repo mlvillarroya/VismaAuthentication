3efd203cedc1dcebb5f703123aa04db801575c82
mlvillarroya <mlvillarroya@gmail.com>
2021-04-20 13:00:58 +0200

    Agregar archivos de proyecto.

---
+++ VismaAuthentication/VismaAuthentication/Constants/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VismaAuthentication.Constants
+{
+    public class Constants
+    {
+        public const string CALLBACK_SCHEME = "xamarinessentials";
+
+    }
+}
